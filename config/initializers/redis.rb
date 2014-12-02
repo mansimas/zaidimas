d645667bda1b6c027bb7cf04d6e11d779e688086
mansimas <norsker@epastas.lt>
2014-12-02 19:26:25 +0100

    succeed deploying redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://127.0.0.1')
-REDIS = Redis.new(:host => uri.host, :port => uri.port || 6379, :password => uri.password, :driver => :hiredis)
+$redis = Redis.new(:host => uri.host, :port => uri.port || 6379, :password => uri.password, :driver => :hiredis)
